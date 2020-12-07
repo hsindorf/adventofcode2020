c299b81d3619e4da8ea6d03d7bb56084f8a5293c
Hannah Sindorf <hannah@hsindorf.com>
2020-12-06 18:35:56 -0800

    finish day 5

--- src/day-5/problem2.test.ts
+++ src/day-5/problem2.test.ts
@@ -1,11 +1,7 @@
-import { getBoardingRow } from './problem2';
+import { getSeat } from './problem2';
 
 describe('Boarding', () => {
-  test.skip('works with a sample input', () => {
-    expect(getBoardingRow('sample-input')).toEqual(44);
-  });
-
-  test.skip('works with a full input', () => {
-    expect(getBoardingRow('input')).toEqual(999);
+  test('works with a full input', () => {
+    expect(getSeat('input')).toEqual(642);
   });
 });
--- src/day-5/problem2.ts
+++ src/day-5/problem2.ts
@@ -1,6 +1,21 @@
 import { getInput } from './getInput';
+import { getSeatId } from './problem1';
 
-export function getBoardingRow(filename: string): number {
+export function getSeat(filename: string): number {
   const data = getInput(filename);
+  const seats = [];
+
+  for (const boardingPass of data) {
+    const seatId = getSeatId(boardingPass);
+    seats.push(seatId);
+  }
+  seats.sort((a, b) => a - b);
+
+  for (let i = 1; i < seats.length; i++) {
+    if (seats[i] !== seats[i - 1] + 1) {
+      return seats[i] - 1;
+    }
+  }
+
   return 0;
 }
